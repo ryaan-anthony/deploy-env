559895012972f1cefa371ec4914164b636bdd6cd
ryaan-anthony <rtulino@gmail.com>
2020-04-05 09:08:42 -0400

    add try?

--- lib/power-rake/common.rb
+++ lib/power-rake/common.rb
@@ -2,8 +2,8 @@
 
 # Prompt with control flow
 # @param [String]
-def continue?(message)
-  abort 'Aborted!' unless prompt("#{message} (y|n)").downcase == 'y'
+def continue?(message = 'Continue? (y|n)')
+  abort 'Aborted!' unless prompt(message).downcase == 'y'
 end
 
 # Inline message prompt
@@ -17,4 +17,12 @@ end
 # @param [string]
 def try(command)
   system(command) || abort("Aborted! `#{command}`")
+end
+
+# Prompt before trying a system command
+# @param [string]
+def try?(command)
+  puts command
+  continue?
+  try(command)
 end
