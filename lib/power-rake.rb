437ea8a8b41eae19c08cbc311379168c88bdd677
ryaan-anthony <rtulino@gmail.com>
2020-04-06 19:25:44 -0400

    update import path

--- lib/power-rake.rb
+++ lib/power-rake.rb
@@ -1,2 +1,2 @@
 # frozen_string_literal: true
-Rake.application.add_import(File.expand_path('lib/power-rake/common.rb'))
+Rake.application.add_import(File.dirname(__FILE__) + '/power-rake/common.rb')
